d4936cec84d5a33154fb7634cd5b835e53a435b4
theGouge <thegouge42@gmail.com>
2018-08-21 12:42:50 -0700

    Added first solution

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // Instructions Go Here
 
 export function rot13(str) {
-  // Code Goes Here
+  const orig = str.split("");
+
+  let rot = orig.map((char) => {
+    let code = char.charCodeAt();
+    if (code >= 65 && code <= 90) {
+      code += 13;
+      if (code > 90) {
+        code -= 26;
+      }
+      return String.fromCharCode(code);
+    } else {
+      return char;
+    }
+  });
+
+  return rot.join("");
 }
 
 const input = document.getElementById("input");
